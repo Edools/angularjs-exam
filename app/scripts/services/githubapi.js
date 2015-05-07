440f744c699cf2db36d495cb429b9a588ab8dfb7
Dayany Espindola <dayanyrec@gmail.com>
2015-05-07 19:41:16 -0300

    Add search repos

--- app/scripts/services/githubapi.js
+++ app/scripts/services/githubapi.js
@@ -9,22 +9,19 @@
  */
 angular.module('myghApp')
   .factory('GithubApi', ['$http', function ($http) {
-    // Service logic
-    // ...
-
-    var meaningOfLife = 42;
-
-    // Public API here
     return {
-      someMethod: function () {
-        return meaningOfLife;
-      },
       searchUsers: function (q) {
         var url = 'https://api.github.com/search/users';
         return $http.get(url, {
           params: {
             q: q
           }
+        });
+      },
+      searchRepo: function (user) {
+        var url = 'https://api.github.com/users/' + user + '/repos';
+        return $http.get(url, {
+          params: {}
         });
       }
     };
--- app/scripts/services/githubmodel.js
+++ app/scripts/services/githubmodel.js
@@ -14,19 +14,49 @@ angular.module('myghApp')
       searchUser: false,
       users: [],
       showUsers: false,
-      user: null
+      user: null,
+      searchRepo: false,
+      repos: [],
+      repo: null
     };
+
     gm.searchUserName = function () {
       gm.searchUser = true;
 
       GithubApi.searchUsers(gm.userName)
         .success(function (data) {
+          gm.searchUser = true;
           gm.users = data.items;
-          console.log(data);
+          gm.showUsers = true;
         })
         .error(function () {
-          console.error('GET ERROR');
+          console.error('USERS GET ERROR');
         });
+
+      console.log('searchUserName');
+    };
+
+    gm.setUser = function (user) {
+      gm.user = user;
+      gm.userName = user.login;
+      gm.showUsers = false;
+
+      console.log('setUser');
+    };
+
+    gm.searchUserRepos = function () {
+      gm.searchRepo = true;
+
+      GithubApi.searchRepo(gm.user.login)
+        .success(function (data) {
+          gm.searchRepo = true;
+          gm.repos = data;
+        })
+        .error(function () {
+          console.error('REPOS GET ERROR');
+        });
+
+      console.log('searchUserRepos');
     };
 
     return gm;

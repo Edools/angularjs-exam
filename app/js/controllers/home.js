8edaaf747bebfbeac99c539a575da038df8e5509
hugo dutra <hugow.dutra@gmail.com>
2015-05-07 01:11:18 -0300

    internet não coopera/faltando adicionar campo para comentar issue

--- app/js/controllers/home.js
+++ app/js/controllers/home.js
@@ -1,3 +1,38 @@
 app.controller('homeCtrl', function($scope, repositoriesFactory){
 
+	$scope.showSearch = false;
+
+    
+    $scope.callRepos = function(org){
+    	$scope.reposList = [];
+
+	    repositoriesFactory.callRepos(org).then(function(data){
+	    	console.log(data);
+	    	$scope.showSearch = true;
+			$scope.reposList = data;
+	    });
+
+    }
+    
+    $scope.teste = function(org){
+    	$scope.reposList = [];
+
+	    repositoriesFactory.teste(org).then(function(data){
+	    	console.log(data);
+	    	$scope.showSearch = true;
+			$scope.reposList = data;
+	    });
+
+    }
+
+    $scope.getIssues = function(owner, repo){
+    	$scope.issuesList = [];
+
+	    repositoriesFactory.getIssues(owner, repo).then(function(data) {
+	    	console.log(data);
+	    	$scope.showIssues = true;
+	    	$scope.issuesList = data;
+	    });
+    }
+
 });
--- app/js/factories/repositories.js
+++ app/js/factories/repositories.js
@@ -1,18 +1,50 @@
 app.factory('repositoriesFactory', function($http, $q){
     var service = {};
     
-    service.call = function(){
+    service.callRepos = function(org){
         var deferred = $q.defer();
-        
+
         $http({
             method: 'GET',
-            url: 'https://api.github.com/orgs/Edools/repos'
+            url: 'https://api.github.com/orgs/' + org + '/repos'
         }).success(function(data){
             deferred.resolve(data);
+        }).error(function(){
+            alert('Ops... Tivemos um problema, tente novamente')
+        });
+        
+        return deferred.promise;
+    }    
+    service.teste = function(org){
+        var deferred = $q.defer();
+
+        $http({
+            method: 'GET',
+            url: 'https://api.github.com/users/' + org + '/repos'
+        }).success(function(data){
+            deferred.resolve(data);
+        }).error(function(){
+            alert('Ops... Tivemos um problema, tente novamente')
         });
         
         return deferred.promise;
     }
     
+    service.getIssues = function(owner, repo) {
+        var deferred = $q.defer();
+
+        $http({
+            method: 'GET',
+            url: 'https://api.github.com/repos/' + owner + '/' + repo + '/issues'
+        }).success(function(data){
+            deferred.resolve(data);
+        }).error(function(){
+            alert('Ops... Tivemos um problema, tente novamente')
+        });
+
+        return deferred.promise;
+
+    }
+
     return service;
 });

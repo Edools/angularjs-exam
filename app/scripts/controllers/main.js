4c189cc1038faa0bf2a5e4990178c1ae20515a50
Dayany Espindola <dayanyrec@gmail.com>
2015-05-07 12:17:09 -0300

    Include users into view

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,43 @@
  * Controller of the myghApp
  */
 angular.module('myghApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('MainCtrl', [function () {
+    var self = this;
+
+    self.user = '';
+
+    self.users = [
+      {
+        'login': 'html5',
+        'id': 0,
+        'name': 'HTML5 Boilerplate',
+        'public_repos': 10,
+        'public_gists': 100
+      },
+      {
+        'login': 'html5',
+        'id': 1,
+        'name': 'AngularJS',
+        'public_repos': 10,
+        'public_gists': 100
+      },
+      {
+        'login': 'html5',
+        'id': 2,
+        'name': 'Yeoman',
+        'public_repos': 10,
+        'public_gists': 100
+      },
+      {
+        'login': 'html5',
+        'id': 3,
+        'name': 'Karma',
+        'public_repos': 10,
+        'public_gists': 100
+      }
     ];
-  });
+
+    self.setUser = function (id) {
+      self.user = self.users[id];
+    };
+  }]);

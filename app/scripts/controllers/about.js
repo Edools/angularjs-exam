3f85506926303534335f36287464fb4c9c73d731
João Mello <joao.mello@uniriotec.br>
2015-09-05 16:55:53 -0300

    Configurando o ambiente

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name queroTrabalharNoEdoolsFrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the queroTrabalharNoEdoolsFrontendApp
+ */
+angular.module('queroTrabalharNoEdoolsFrontendApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name queroTrabalharNoEdoolsFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the queroTrabalharNoEdoolsFrontendApp
+ */
+angular.module('queroTrabalharNoEdoolsFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

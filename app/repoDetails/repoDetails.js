16ea4ee2d5bef3535147efc0794277af82cc53bc
Rodrigo Gouvêa <rodrigosggouvea@gmail.com>
2015-09-06 23:44:30 -0300

    issues list

--- app/repoDetails/repoDetails.js
+++ app/repoDetails/repoDetails.js
@@ -16,14 +16,23 @@ angular.module('myApp.repoDetails', ['ngRoute'])
       url: "https://api.github.com/repos/"+full_name
     });
   };
+  githubAPI.getRepoIssues = function(full_name){
+    return $http({
+      url: "https://api.github.com/repos/"+full_name+"/issues"
+    });
+  }
   return githubAPI;
 })
 
 .controller('repoDetailsController', function($scope, $routeParams, repoDetailsService) {
   $scope.full_name = $routeParams.owner + '/' + $routeParams.repo
   $scope.repo = null;
+  $scope.issuesList = [];
   repoDetailsService.getRepoDetails($scope.full_name).success(function(response){
     $scope.repo = response;
+  });
+  repoDetailsService.getRepoIssues($scope.full_name).success(function(response){
+    $scope.issuesList = response;
   });
 })
 

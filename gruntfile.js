f84bdc415fb325cd67bd298f4563a5a53637f87c
Bernardo (John) <bernardo.bt@gmail.com>
2015-09-16 21:52:19 -0300

    Limpando o gruntfile para ficar somente com as tasks utilizadas no projeto: sass, watch e browserSync

--- gruntfile.js
+++ gruntfile.js
@@ -4,61 +4,6 @@ module.exports = function(grunt){
 
         grunt.initConfig({
 
-            copy: {
-                public: {
-                    cwd: 'public',      //nome da pasta a ser copiada
-                    src: '**',          //todos os arquivos e diretórios são copiados
-                    dest: 'dist',       //nome da pasta destino
-                    expand: true        //ativa mapeamento dinâmico -
-                                        //torma no pocesso de configuração do copy mais simples
-                }
-            },
-
-            clean: {
-                dist:{
-                    src: 'dist'
-                },
-
-                scss:{
-                    src: 'dist/scss'
-                }
-            },
-
-            useminPrepare:{
-                html:'dist/**/*.html'
-            },
-
-            usemin:{                            //importante para alterar referencias:
-                html:'dist/**/*.html',          //  - no html
-                css: 'dist/**/*.min.css',       //  - no CSS
-                js: 'dist/**/*.min.js'          //  - no js
-            },
-
-            imagemin: {
-                public:{
-                    cwd: 'public',
-                    src: '**/*.{png,jpg,gif}',
-                    dest: 'dist',
-                    expand: true
-                }
-            },
-
-            rev:{
-                options:{
-                    enconding:'uft8',
-                    algorithm:'md5',
-                    length:8
-                },
-
-                imagens: {
-                    src:['dist/img/**/*.{png,jpg,gif}']
-                },
-
-                minificados: {
-                    src:['dist/css/**/*.min.css', 'dist/js/**/*.min.js']
-                }
-            },
-
             watch:{
                 sass:{
                     files:'public/scss/**/*.scss',
@@ -98,24 +43,9 @@ module.exports = function(grunt){
 
         grunt.registerTask('server',['browserSync','watch']);
 
-        grunt.registerTask('publicar',['clean:dist','copy','clean:scss']);
-        grunt.registerTask('minificar', ['useminPrepare',
-                                         'concat', 'uglify', 'cssmin',
-                                         'rev',
-                                         'usemin',
-                                         'imagemin']);
-
-        grunt.registerTask('default',['publicar','minificar']);
 
     //Importando os pacotes
-        grunt.loadNpmTasks('grunt-contrib-copy');
-        grunt.loadNpmTasks('grunt-contrib-clean');
-        grunt.loadNpmTasks('grunt-contrib-concat');
-        grunt.loadNpmTasks('grunt-contrib-uglify');
-        grunt.loadNpmTasks('grunt-contrib-cssmin');
-        grunt.loadNpmTasks('grunt-contrib-imagemin');
-        grunt.loadNpmTasks('grunt-usemin');
-        grunt.loadNpmTasks('grunt-rev');
+
         grunt.loadNpmTasks('grunt-contrib-sass');
         grunt.loadNpmTasks('grunt-contrib-watch');
         grunt.loadNpmTasks('grunt-browser-sync');

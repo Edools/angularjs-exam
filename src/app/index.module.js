0d872042e1df3f59e8ff57f86998c452c49602a1
Mayke Sargento de Freitas <mayke_sargento@hotmail.com>
2016-03-01 10:50:32 -0300

    initial commit with dev tasks and app structure

---
+++ src/app/index.module.js
@@ -0,0 +1,15 @@
+/* global malarkey:false, moment:false */
+
+import { config } from './index.config';
+import { routerConfig } from './index.route';
+import { runBlock } from './index.run';
+import { MainController } from './main/main.controller';
+import { NavbarDirective } from '../app/components/navbar/navbar.directive';
+
+angular.module('gitDools', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr'])
+  .constant('moment', moment)
+  .config(config)
+  .config(routerConfig)
+  .run(runBlock)
+  .controller('MainController', MainController)
+  .directive('navbar', NavbarDirective);
---
+++ src/app/main/main.controller.js
@@ -0,0 +1,7 @@
+export class MainController {
+  constructor (toastr) {
+    'ngInject';
+
+    this.toastr = toastr;
+  }
+}

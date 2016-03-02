370434fcdca9a1cedbc2105901bf163f012147c5
Marco Lima <contato@mlimasolucoes.com.br>
2016-03-02 03:56:52 -0300

    project start

---
+++ src/features/home/home.controller.test.js
@@ -0,0 +1,12 @@
+import home from './index';
+
+describe('Controller: Home', function() {
+  let $controller;
+
+  beforeEach(angular.mock.module(home));
+
+  beforeEach(angular.mock.inject(function(_$controller_) {
+    $controller = _$controller_;
+  }));
+
+});

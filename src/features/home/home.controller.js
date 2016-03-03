0004af1138478a6e12c570116c5625bbb418c376
Marco Lima <contato@mlimasolucoes.com.br>
2016-03-03 17:40:53 -0300

    test api call

--- src/features/home/home.controller.js
+++ src/features/home/home.controller.js
@@ -1,8 +1,9 @@
 export default class HomeController {
   constructor(elasticGit) {
     this.elasticGit = elasticGit;
-    elasticGit.getRepos().then(result => this.repos = result.data);
+    elasticGit.getRepos().then(result => {this.repos = result.data; this.successApiCall = "Ok";} );
   }
+
 }
 
 HomeController.$inject = ['elasticGit'];
--- src/features/home/home.controller.test.js
+++ src/features/home/home.controller.test.js
@@ -1,12 +1,21 @@
 import home from './index';
 
 describe('Controller: Home', function() {
-  let $controller;
+  let $controller, $timeout;
 
   beforeEach(angular.mock.module(home));
 
-  beforeEach(angular.mock.inject(function(_$controller_) {
+  beforeEach(angular.mock.inject(function(_$controller_, _$timeout_) {
     $controller = _$controller_;
+    $timeout = _$timeout_;
   }));
+
+  it('Repositories loaded!', function() {
+    let ctrl = $controller('HomeController');
+    $timeout(function() {
+        expect(ctrl.elasticGit.successApiCall).toBe("Ok");
+    }, 1000);
+    //expect(ctrl.elasticGit.getRepos).not.toBe(null);
+  });
 
 });

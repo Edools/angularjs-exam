a7ac27afa722ab3efd85d3e3d4b2390ebe2718b9
Marco Lima <contato@mlimasolucoes.com.br>
2016-03-03 18:22:25 -0300

    get commits from repo

--- src/features/home/home.controller.js
+++ src/features/home/home.controller.js
@@ -1,7 +1,33 @@
 export default class HomeController {
   constructor(elasticGit) {
     this.elasticGit = elasticGit;
-    elasticGit.getRepos().then(result => {this.repos = result.data; this.successApiCall = "Ok";} );
+    elasticGit.getRepos().then(result => {this.repos = result.data; this.successApiCall = "Ok";});
+  }
+
+  loadCommit(repo) {
+  	//this.elasticGit.getCommit(repo);
+
+  	let page = 0;
+  	
+  	//counter increment todo load more
+  	page++;
+
+  	//populate name
+  	this.name = repo.name;
+
+  	//populate forks
+  	this.forks = repo.forks_count;
+
+  	//populate stars
+  	this.stars = repo.stargazers_count;
+
+  	//commit url with pagination
+	let commit_url = "https://api.github.com/repositories/" + repo.id + "/commits?page="+page+"&per_page=20&"+this.secret_token;
+
+	this.elasticGit.getCommit(commit_url).then(result => {
+		this.commit = result.data;
+	});
+
   }
 
 }
--- src/services/home.service.js
+++ src/services/home.service.js
@@ -1,13 +1,18 @@
 import angular from 'angular';
 
 class ElasticGit {
+
   constructor($http) {
     this.$http = $http;
+    this.secret_token = 'client_id=0c278b28c4cf46e2e3bc&client_secret=783526f3612d6a832e783f171bd2af28b779c263';
   }
 
   getRepos() {
-    let secret_token = 'client_id=0c278b28c4cf46e2e3bc&client_secret=783526f3612d6a832e783f171bd2af28b779c263';
-    return this.$http.get('https://api.github.com/users/elastic/repos?'+secret_token);
+    return this.$http.get('https://api.github.com/users/elastic/repos?'+this.secret_token);
+  }
+
+  getCommit(commit_url) {
+	return this.$http.get(commit_url);
   }
 }
 

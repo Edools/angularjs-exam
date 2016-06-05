76d82ca81d75191d64f08fbc747b6e1c0e552ced
Guilherme Gonçalves <inacio.guilherme@gmail.com>
2016-06-05 18:24:17 -0300

    Atualizando about.

--- src/app/components/githubContributor/index.js
+++
@@ -1,5 +0,0 @@
-import { GithubContributorService } from './githubContributor.service';
-
-export default angular
-  .module('app.components.githubContributor', [])
-  .service('githubContributor', GithubContributorService);

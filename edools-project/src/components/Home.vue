8fd186826975e095b6fdbc1accef0f6c7546b672
Diogo Neves <diogooncastro1@gmail.com>
2018-05-08 17:06:01 -0300

    Added School Features table.

--- edools-project/src/components/Home.vue
+++ edools-project/src/components/Home.vue
@@ -2,80 +2,93 @@
   <v-container>
     <v-layout row wrap>
       <v-flex xs12>
-        <h1 class="welcome">BEM VINDO AO APP DA EDOOLS!</h1>
-      </v-flex>
-      <v-flex xs12>
-        <h3 class="welcome">Aqui você poderá visualizar todos os aplicativos disponíveis.</h3>
+        <h1 class="welcome">BEM VINDO AO PAINEL DO APP DA EDOOLS!</h1>
       </v-flex>
     </v-layout>
     <v-layout row wrap>
       <v-flex xs12>
-        <!-- <v-card>
-          <v-card-title>
-            Aplicativos
-            <v-spacer></v-spacer>
-            <v-text-field
-              append-icon="search"
-              label="Search"
-              single-line
-              hide-details
-              v-model="search"
-            ></v-text-field>
-          </v-card-title>
-          <v-data-table
-                :pagination.sync="pagination"
-                :headers="headers"
-                :items="featureItems"
-                :search="search"
-                rows-per-page-text="Linhas por página"
-                no-data-text="Sem dados disponíveis"
-                no-results-text="Sem resultados para a sua pesquisa"
-              >
-              <template slot="items" slot-scope="props">
-                <td>
-                  <span> {{ props.item.feature_key }} </span>
-                </td>
-                <td>
-                  <span> {{ props.item.id }} </span>
-                </td>
-              </template>
-              <template  slot="pageText" slot-scope="{ pageStart, pageStop }">
-                De {{ pageStart }} à {{ pageStop }}
-              </template>
-            </v-data-table>
-        </v-card> -->
+        <h3 class="small">Aqui você poderá visualizar todos os aplicativos disponíveis.</h3>
+      </v-flex>
+      <v-flex xs12>
+          <v-expansion-panel popout>
+            <v-expansion-panel-content>
+              <div class="text-xs-center" slot="header">Aplicativos</div>
+              <v-card>
+                <v-card-title>
+                  <v-text-field
+                    v-model="search"
+                    append-icon="search"
+                    label="Search"
+                    single-line
+                    hide-details
+                  ></v-text-field>
+                </v-card-title>
+                <v-data-table
+                  :headers="featureHeaders"
+                  :items="featureItems"
+                  :search="search"
+                  rows-per-page-text="Linhas por página"
+                  no-data-text="Sem dados disponíveis"
+                >
+                  <template slot="items" slot-scope="props">
+                    <td class="text-xs-left">{{ props.item.id }}</td>
+                    <td>{{ props.item.feature_key }}</td>
+                  </template>
+                  <template  slot="pageText" slot-scope="{ pageStart, pageStop }">
+                    De {{ pageStart }} à {{ pageStop }}
+                  </template>
+                  <v-alert slot="no-results" :value="true" color="error" icon="warning">
+                    Sua pesquisa por "{{ search }}" não obteve resultados.
+                  </v-alert>
+                </v-data-table>
+              </v-card>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-flex>
+    </v-layout>
 
-          <v-card>
-            <v-card-title>
-              Aplicativos
-              <v-spacer></v-spacer>
-              <v-text-field
-                v-model="search"
-                append-icon="search"
-                label="Search"
-                single-line
-                hide-details
-              ></v-text-field>
-            </v-card-title>
-            <v-data-table
-              :headers="featureHeaders"
-              :items="featureItems"
-              :search="search"
-              rows-per-page-text="Linhas por página"
-              no-data-text="Sem dados disponíveis"
-            >
-              <template slot="items" slot-scope="props">
-                <td class="text-xs-left">{{ props.item.id }}</td>
-                <td>{{ props.item.feature_key }}</td>
-              </template>
-              <template  slot="pageText" slot-scope="{ pageStart, pageStop }">
-                De {{ pageStart }} à {{ pageStop }}
-              </template>
-              <v-alert slot="no-results" :value="true" color="error" icon="warning">
-                Infelizmente sua pesquisa por "{{ search }}" não obteve resultados.
-              </v-alert>
-            </v-data-table>
-          </v-card>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <h3 class="small">Aqui você poderá visualizar todas as features da escola selecionada.</h3>
+      </v-flex>
+      <v-flex xs12>
+          <v-expansion-panel popout>
+            <v-expansion-panel-content>
+              <div class="text-xs-center" slot="header">Recursos da Escola</div>
+              <v-card>
+                <v-card-title>
+                  <v-text-field
+                    v-model="search"
+                    append-icon="search"
+                    label="Search"
+                    single-line
+                    hide-details
+                  ></v-text-field>
+                </v-card-title>
+                <v-data-table
+                  :headers="schoolHeaders"
+                  :items="schoolItems"
+                  :search="search"
+                  rows-per-page-text="Linhas por página"
+                  no-data-text="Sem dados disponíveis"
+                >
+                  <template slot="items" slot-scope="props">
+                    <td class="text-xs-left">{{ props.item.id }}</td>
+                    <td>{{ props.item.active }}</td>
+                    <td>{{ props.item.available }}</td>
+                    <td>{{ props.item.feature_id }}</td>
+                    <td><v-switch @click="editFeature" v-model="props.item.active"></v-switch></td>
+                  </template>
+                  <template  slot="pageText" slot-scope="{ pageStart, pageStop }">
+                    De {{ pageStart }} à {{ pageStop }}
+                  </template>
+                  <v-alert slot="no-results" :value="true" color="error" icon="warning">
+                    Sua pesquisa por "{{ search }}" não obteve resultados.
+                  </v-alert>
+                </v-data-table>
+              </v-card>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
       </v-flex>
     </v-layout>
 
@@ -88,6 +101,7 @@ export default {
   data () {
     return {
       search: '',
+      activate: true,
       pagination: {
         rowsPerPage: 20
       },
@@ -104,24 +118,59 @@ export default {
         }
       ],
       featureItems: [],
-      schoolItems: []
+      schoolHeaders: [
+        {
+          text: 'ID',
+          align: 'left',
+          value: 'id'
+        },
+        {
+          text: 'Ativo',
+          align: 'left',
+          value: 'active'
+        },
+        {
+          text: 'Disponível',
+          align: 'left',
+          value: 'available'
+        },
+        {
+          text: 'ID Aplicativo',
+          align: 'left',
+          value: 'featureId'
+        },
+        {
+          text: 'Ativar/Desativar',
+          align: 'left',
+          value: 'activate'
+        }
+      ],
+      schoolItems: [],
+      schoolId: null
     }
   },
   created () {
     axios.get('/features?per_page=' + this.pagination.rowsPerPage)
       .then(response => {
-        console.log(response.data.features)
         this.featureItems = response.data.features
       }).catch(error => {
         console.log(error.config)
       })
     axios.get('/schools/64/school_features?per_page=' + this.pagination.rowsPerPage)
       .then(response => {
-        console.log(response.data.schoolfeatures)
-        this.schoolItems = response.data.id
+        this.schoolItems = response.data.schoolfeatures
+        this.schoolId = response.data.schoolfeatures.id
       }).catch(error => {
         console.log(error.config)
       })
+  },
+  methods: {
+    editFeature () {
+      axios.put('/school_features/' + this.schoolId + '/deactivate', {active: this.schoolFeatures})
+        .then(response => {
+          this.schoolFeatures.active = false
+        })
+    }
   }
 }
 </script>
@@ -129,5 +178,11 @@ export default {
 <style scoped>
 .welcome {
   text-align: center;
+  padding-bottom: 10%;
+}
+.small {
+  text-align: center;
+  font-size: 0.8rem;
+  padding-top: 5%;
 }
 </style>

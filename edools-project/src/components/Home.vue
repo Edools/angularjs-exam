947d337a64b1ef037c167d35e8be147aaee8697b
Diogo Neves <diogooncastro1@gmail.com>
2018-05-09 16:32:10 -0300

    Changing value from features.

--- edools-project/src/components/Home.vue
+++ edools-project/src/components/Home.vue
@@ -14,15 +14,6 @@
             <v-expansion-panel-content>
               <div class="text-xs-center" slot="header">Aplicativos</div>
               <v-card>
-                <v-card-title>
-                  <v-text-field
-                    v-model="search"
-                    append-icon="search"
-                    label="Search"
-                    single-line
-                    hide-details
-                  ></v-text-field>
-                </v-card-title>
                 <v-data-table
                   :headers="featureHeaders"
                   :items="featureItems"
@@ -37,9 +28,6 @@
                   <template  slot="pageText" slot-scope="{ pageStart, pageStop }">
                     De {{ pageStart }} à {{ pageStop }}
                   </template>
-                  <v-alert slot="no-results" :value="true" color="error" icon="warning">
-                    Sua pesquisa por "{{ search }}" não obteve resultados.
-                  </v-alert>
                 </v-data-table>
               </v-card>
           </v-expansion-panel-content>
@@ -49,22 +37,13 @@
 
     <v-layout row wrap>
       <v-flex xs12>
-        <h3 class="small">Aqui você poderá visualizar todas as features da escola selecionada.</h3>
+        <h3 class="small">Aqui você poderá editar todas as features da escola selecionada.</h3>
       </v-flex>
       <v-flex xs12>
           <v-expansion-panel popout>
             <v-expansion-panel-content>
               <div class="text-xs-center" slot="header">Recursos da Escola</div>
               <v-card>
-                <v-card-title>
-                  <v-text-field
-                    v-model="search"
-                    append-icon="search"
-                    label="Search"
-                    single-line
-                    hide-details
-                  ></v-text-field>
-                </v-card-title>
                 <v-data-table
                   :headers="schoolHeaders"
                   :items="schoolItems"
@@ -77,14 +56,11 @@
                     <td>{{ props.item.active }}</td>
                     <td>{{ props.item.available }}</td>
                     <td>{{ props.item.feature_id }}</td>
-                    <td><v-switch @click="editFeature" v-model="props.item.active"></v-switch></td>
+                    <td><v-switch @click="editFeature(props.item.id)" v-model="props.item.active"></v-switch></td>
                   </template>
                   <template  slot="pageText" slot-scope="{ pageStart, pageStop }">
                     De {{ pageStart }} à {{ pageStop }}
                   </template>
-                  <v-alert slot="no-results" :value="true" color="error" icon="warning">
-                    Sua pesquisa por "{{ search }}" não obteve resultados.
-                  </v-alert>
                 </v-data-table>
               </v-card>
           </v-expansion-panel-content>
@@ -100,7 +76,7 @@ import axios from 'axios'
 export default {
   data () {
     return {
-      search: '',
+      search: null,
       activate: true,
       pagination: {
         rowsPerPage: 20
@@ -146,29 +122,31 @@ export default {
         }
       ],
       schoolItems: [],
-      schoolId: null
+      schoolId: []
     }
   },
-  created () {
+  mounted () {
     axios.get('/features?per_page=' + this.pagination.rowsPerPage)
       .then(response => {
         this.featureItems = response.data.features
+        console.log(this.featureItems)
       }).catch(error => {
         console.log(error.config)
       })
     axios.get('/schools/64/school_features?per_page=' + this.pagination.rowsPerPage)
       .then(response => {
         this.schoolItems = response.data.schoolfeatures
-        this.schoolId = response.data.schoolfeatures.id
       }).catch(error => {
         console.log(error.config)
       })
   },
   methods: {
-    editFeature () {
-      axios.put('/school_features/' + this.schoolId + '/deactivate', {active: this.schoolFeatures})
+    editFeature (id) {
+      axios.put('/school_features/' + id + '/deactivate', {active: this.schoolFeatures})
         .then(response => {
-          this.schoolFeatures.active = false
+        })
+      axios.put('/school_features/' + id + '/activate', {active: this.schoolFeatures})
+        .then(response => {
         })
     }
   }
@@ -177,6 +155,7 @@ export default {
 
 <style scoped>
 .welcome {
+  color: #48a978;
   text-align: center;
   padding-bottom: 10%;
 }

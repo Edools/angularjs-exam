0636d559825f07f778ce7de87f61e2e2598ca6f3
Diogo Neves <diogooncastro1@gmail.com>
2018-05-11 08:37:31 -0300

    Server configuration to Heroku

--- edools-project/server.js
+++ edools-project/server.js
@@ -4,8 +4,9 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
-app.listen(port);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 app.get('*', (request, response) => {
 	response.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
